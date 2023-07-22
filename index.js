f73985ba723763ee3d5365387b1d5f12f04e18d3
cjrdelosreyes <122694227+cjroma0199@users.noreply.github.com>
2023-07-23 04:24:55 +0800

    Clean code a little bit

--- index.js
+++ index.js
@@ -10,36 +10,43 @@ const isButtonActive = (button) => {
   return Object.values(button.classList).includes("active-mode");
 };
 
-const removeActiveButton = (button) => {};
+function removeActiveButton(button) {
+  buttons.forEach((btn) => {
+    btn.classList.remove("active-mode");
+    if (btn != button) btn.style.border = "none";
+  });
+}
 
-const setActiveButton = (button) => {};
+const setActiveButton = (button) => {
+  button.classList.add("active-mode");
+};
+
+function hoverOut() {
+  if (!isButtonActive(this)) this.style.border = "none";
+}
+
+function hoverOver() {
+  if (this.id != "rainbow") {
+    this.style.borderLeft = `8px solid ${colorInput.value}`;
+    return;
+  }
+  this.style.cssText = `border-left: 8px solid transparent;
+    border-image: linear-gradient(
+    to top,
+    #b827fc 0%,
+    #2c90fc 25%,
+    #b8fd33 50%,
+    #fec837 75%,
+    #fd1892 100%);
+    border-image-slice: 1;`;
+}
 
 buttons.forEach((button) => {
-  button.addEventListener("mouseover", function () {
-    if (this.id != "rainbow") {
-      this.style.borderLeft = `8px solid ${colorInput.value}`;
-      return;
-    }
-    this.style.cssText = `border-left: 8px solid transparent;
-      border-image: linear-gradient(
-      to top,
-      #b827fc 0%,
-      #2c90fc 25%,
-      #b8fd33 50%,
-      #fec837 75%,
-      #fd1892 100%);
-      border-image-slice: 1;`;
-  });
-
-  button.addEventListener("mouseout", function () {
-    if (!isButtonActive(button)) this.style.border = "none";
-  });
+  button.addEventListener("mouseover", hoverOver);
+  button.addEventListener("mouseout", hoverOut);
 
   button.addEventListener("click", function () {
-    buttons.forEach((btn) => {
-      btn.classList.remove("active-mode");
-      if (btn != button) btn.style.border = "none";
-    });
-    this.classList.add("active-mode");
+    removeActiveButton(this);
+    setActiveButton(this);
   });
 });
